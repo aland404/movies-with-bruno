dcab56c3e950bb3d50fcef499c191d7a20a8e279
Alan Duchêne <alan.duchene@zenika.com>
2024-05-02 09:31:01 +0200

    feat: init user authent

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import {Module} from '@nestjs/common';
 import {MovieController} from "./movie/infrastructure/movie.controller";
 import {InMemoryMovieRepository} from "./movie/infrastructure/movie.repository";
 import {MovieRepository} from "./movie/domain/movieRepository.interface";
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule],
   controllers: [MovieController],
   providers: [{provide: MovieRepository, useClass: InMemoryMovieRepository}],
 })
