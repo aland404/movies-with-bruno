b6bf72f46f1b074022ac6b44855c9b82df7423e2
Alan Duchêne <alan.duchene@zenika.com>
2024-05-14 19:12:50 +0200

    handle auth with token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,8 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {Body, Controller, Get, HttpCode, HttpStatus, Post, Req, UseGuards} from '@nestjs/common';
+import {AuthService} from './auth.service';
+import {AuthGuard} from "./auth.guard";
+import {Request} from "express";
+
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +12,11 @@ export class AuthController {
     @Post('login')
     signIn(@Body() signInDto: Record<string, any>) {
         return this.authService.signIn(signInDto.username, signInDto.password);
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getProfile(@Req() req: Request) {
+        return req.user;
     }
 }
