ae80831bdfb6da35f19b4d8d9b40b00df43e201b
Alan Duchêne <alan.duchene@zenika.com>
2024-09-27 06:29:18 +0200

    chore: add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
-import {Module} from '@nestjs/common';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import {MovieController} from "./movie/infrastructure/controller/movie.controller";
 import {InMemoryMovieRepository} from "./movie/infrastructure/movie.repository";
 import {MovieRepository} from "./movie/domain/movieRepository.interface";
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AppLoggerMiddleware } from "./infra/middlewares";
 
 @Module({
   imports: [AuthModule, UsersModule],
   controllers: [MovieController],
   providers: [{provide: MovieRepository, useClass: InMemoryMovieRepository}],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(AppLoggerMiddleware).forRoutes('*')
+  }
+}
---
+++ src/infra/middlewares/AppLoggerMiddleware.ts
@@ -0,0 +1,24 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common'
+
+import { NextFunction, Request, Response } from 'express'
+
+@Injectable()
+export class AppLoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP')
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, baseUrl: url, body } = request
+
+    const userAgent = request.get('user-agent') || ''
+
+    response.on('close', () => {
+      const { statusCode } = response
+      const contentLength = response.get('content-length')
+
+      this.logger.log(`${method} ${ip}${url} || statusCode: ${statusCode} || contentLength: ${contentLength} || userAgent: ${userAgent}`)
+      if(body) this.logger.log(`Body used : `, body)
+    })
+
+    next()
+  }
+}
