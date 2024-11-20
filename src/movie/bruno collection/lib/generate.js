c58191ad5c9253efe02c8130d15651868a44ea5b
Alan Duchêne <duchene.alan56@gmail.com>
2024-11-20 16:56:48 +0100

    feat(movie-bru): update requests and environments

--- src/movie/bruno collection/lib/generate.js
+++ src/movie/bruno collection/lib/generate.js
@@ -1,29 +1,29 @@
 const { randMovie, randFirstName, randLastName, randPastDate } = require('@ngneat/falso')
-const { toSeparatedHyphens } = require("./transform")
+const { toSeparatedHyphens } = require('./transform')
 
 function generateRandomMovieToPost(bodyToPost = {}) {
-    const randomTitle = randMovie()
+  const randomTitle = randMovie()
 
-    return {
-        title: bodyToPost.title ??  randomTitle,
-        slug: bodyToPost.slug ??  toSeparatedHyphens(randomTitle),
-        actors: bodyToPost.actors ?? `${randFirstName()} ${randLastName()}`,
-        boxOffice: bodyToPost.boxOffice ??  "My boxOffice",
-        director: bodyToPost.director ??  "My director",
-        genre: bodyToPost.genre ??  "My genre",
-        plot: bodyToPost.plot ??  "My plot",
-        runtime: bodyToPost.runtime ??  "My runtime",
-        type: bodyToPost.type ??  "My type",
-        writer: bodyToPost.writer ??  `${randFirstName()} ${randLastName()}`,
-        year: bodyToPost.year ??  randPastDate({ years: 20 }).getFullYear().toString(),
-        awards: bodyToPost.awards ??  "My awards",
-        imdbRating: bodyToPost.imdbRating ??  "My imdbRating",
-        imdbVotes: bodyToPost.imdbVotes ??  "My imdbVotes",
-        poster: bodyToPost.poster ??  "My poster",
-        released: bodyToPost.released ??  "My released"
-    }
+  return {
+    title: bodyToPost.title ?? randomTitle,
+    slug: bodyToPost.slug ?? toSeparatedHyphens(bodyToPost.title ?? randomTitle),
+    actors: bodyToPost.actors ?? `${randFirstName()} ${randLastName()}`,
+    boxOffice: bodyToPost.boxOffice ?? 'My boxOffice',
+    director: bodyToPost.director ?? 'My director',
+    genre: bodyToPost.genre ?? 'My genre',
+    plot: bodyToPost.plot ?? 'My plot',
+    runtime: bodyToPost.runtime ?? 'My runtime',
+    type: bodyToPost.type ?? 'My type',
+    writer: bodyToPost.writer ?? `${randFirstName()} ${randLastName()}`,
+    year: bodyToPost.year ?? randPastDate({ years: 20 }).getFullYear().toString(),
+    awards: bodyToPost.awards ?? 'My awards',
+    imdbRating: bodyToPost.imdbRating ?? 'My imdbRating',
+    imdbVotes: bodyToPost.imdbVotes ?? 'My imdbVotes',
+    poster: bodyToPost.poster ?? 'My poster',
+    released: bodyToPost.released ?? 'My released',
+  }
 }
 
 module.exports = {
-    generateRandomMovieToPost
+  generateRandomMovieToPost,
 }
--- src/movie/bruno collection/lib/transform.js
+++ src/movie/bruno collection/lib/transform.js
@@ -1,10 +1,10 @@
 function toSeparatedHyphens(sentence) {
-    const lowerCaseSentence = sentence.toLowerCase()
-    const hyphenSeparated = lowerCaseSentence.replace(/\s+/g, '-')
+  const lowerCaseSentence = sentence.toLowerCase()
+  const hyphenSeparated = lowerCaseSentence.replace(/\s+/g, '-')
 
-    return hyphenSeparated;
+  return hyphenSeparated
 }
 
 module.exports = {
-    toSeparatedHyphens
+  toSeparatedHyphens,
 }
